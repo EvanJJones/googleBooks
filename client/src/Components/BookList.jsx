b8f15c043f3be70c39f17bf536dc9cb99febb813
Evan Jones <evanjones@fastmail.net>
2020-01-03 11:47:21 -0500

    added comments

--- client/src/Components/BookList.jsx
+++ client/src/Components/BookList.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import API from '../utils/API';
-import Container from './Container';
+import React from "react";
+import API from "../utils/API";
+import Container from "./Container";
 
 const styles = {
-  width: '100%',
-  height: '80%',
-  display: 'flex',
-  justifyContent: 'space-evenly',
-  flexWrap: 'wrap'
+  width: "100%",
+  height: "80%",
+  display: "flex",
+  justifyContent: "space-evenly",
+  flexWrap: "wrap"
 };
 
 function Books(props) {
-  const saveBook = (book) => {
+  // for saving a new book to db
+  const saveBook = book => {
     const {
       title,
       authors,
@@ -27,24 +28,24 @@ function Books(props) {
       link: infoLink
     };
     if (!imageLinks) {
-      newBook.image = 'https://via.placeholder.com/128x171';
+      newBook.image = "https://via.placeholder.com/128x171";
     } else {
       newBook.image = imageLinks.thumbnail;
     }
 
     API.saveBook(newBook)
-      .then((res) => {})
-      .catch((err) => console.log(err));
+      .then(res => {})
+      .catch(err => console.log(err));
   };
 
   return (
     <div>
-      {props.books.map((book) => (
+      {props.books.map(book => (
         <div className="card" key={book.id}>
           <div className="card-header">
             <a href={book.volumeInfo.infoLink}>
-              <h2>{book.volumeInfo.title}</h2>{' '}
-            </a>{' '}
+              <h2>{book.volumeInfo.title}</h2>{" "}
+            </a>{" "}
             By: {book.volumeInfo.authors}
           </div>
           <div className="card-body">
@@ -54,7 +55,7 @@ function Books(props) {
                 src={
                   book.volumeInfo.imageLinks
                     ? book.volumeInfo.imageLinks.thumbnail
-                    : 'https://via.placeholder.com/128x171'
+                    : "https://via.placeholder.com/128x171"
                 }
               />
               <p className="m-3">{book.volumeInfo.description}</p>

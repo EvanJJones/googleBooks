69008ba4129e42c7cf48afdd7acd9d930baff3b3
Evan Jones <evanjones@fastmail.net>
2020-01-03 11:41:31 -0500

    added comments

--- client/src/Components/Container.jsx
+++ client/src/Components/Container.jsx
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import API from '../utils/API';
-import BookList from './BookList.jsx';
-import Search from './Search.jsx';
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+import BookList from "./BookList.jsx";
+import Search from "./Search.jsx";
 
 const styles = {
-  width: '100%',
-  height: '80%',
-  display: 'flex',
-  justifyContent: 'space-evenly',
-  flexWrap: 'wrap'
+  width: "100%",
+  height: "80%",
+  display: "flex",
+  justifyContent: "space-evenly",
+  flexWrap: "wrap"
 };
 
 function Container(props) {
+  // state that holds the books to display
   const [books, setBooks] = useState([]);
 
-  const searchGoogleBooks = (query) => {
+  const searchGoogleBooks = query => {
     API.search(query)
-      .then((res) => {
+      .then(res => {
         setBooks(res.data.items);
         console.log(res.data.items);
         console.log(res.data.items[0].volumeInfo);
       })
-      .catch((err) => console.log(err));
+      .catch(err => console.log(err));
   };
 
   return (

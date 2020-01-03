363cfa79c73ef8790d064cd88b5d43442e47a368
Evan Jones <evanjones@fastmail.net>
2020-01-03 11:42:45 -0500

    added comments

--- client/src/Components/SavedBookList.jsx
+++ client/src/Components/SavedBookList.jsx
@@ -1,34 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import API from '../utils/API';
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 
 const styles = {
-  width: '100%',
-  height: '80%',
-  display: 'flex',
-  justifyContent: 'space-evenly',
-  flexWrap: 'wrap'
+  width: "100%",
+  height: "80%",
+  display: "flex",
+  justifyContent: "space-evenly",
+  flexWrap: "wrap"
 };
 
 function SavedBookList(props) {
+  // state that will hold the list of saved bookks from db
   const [books, setBooks] = useState([]);
 
   const getSavedBooks = () => {
     API.getSaved()
-      .then((res) => {
+      .then(res => {
         setBooks(res.data);
       })
-      .catch((err) => console.log(err));
+      .catch(err => console.log(err));
   };
 
-  const deleteBook = (id) => {
+  const deleteBook = id => {
     console.log(id);
     API.deleteBook(id)
-      .then((res) => {
-        const filtered = books.filter((book) => book._id !== id);
+      .then(res => {
+        const filtered = books.filter(book => book._id !== id);
         setBooks(filtered);
         console.log(books);
       })
-      .catch((err) => console.log(err));
+      .catch(err => console.log(err));
   };
 
   useEffect(() => {
@@ -38,12 +39,12 @@ function SavedBookList(props) {
 
   return (
     <div>
-      {books.map((book) => (
+      {books.map(book => (
         <div className="card" key={book._id}>
           <div className="card-header">
             <a href={book.link}>
-              <h2>{book.title}</h2>{' '}
-            </a>{' '}
+              <h2>{book.title}</h2>{" "}
+            </a>{" "}
             By: {book.authors}
           </div>
           <div className="card-body">
@@ -53,7 +54,7 @@ function SavedBookList(props) {
                 src={
                   book.image
                     ? book.image
-                    : 'https://via.placeholder.com/128x171'
+                    : "https://via.placeholder.com/128x171"
                 }
               />
               <p className="m-3">{book.description}</p>
